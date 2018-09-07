bc72827d943d4efc1d5cef3366d4858f7ef8e263
cristofervidal <cristofervidal26@gmail.com>
2018-09-06 21:09:47 -0500

    ok
    ok

--- laboratorio060918_Cristofer/src/Clase1/PERSONA.java
+++ laboratorio060918_Cristofer/src/Clase1/PERSONA.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Clase1;
 
 /**
@@ -10,5 +6,39 @@ package Clase1;
  * @author Programacion
  */
 public class PERSONA {
-    
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public double getEstatura() {
+        return estatura;
+    }
+
+    public void setEstatura(double estatura) {
+        this.estatura = estatura;
+    }
+String nombre,apellido;
+double peso,estatura;
+ 
 }
